8605e676731fea296f4859e61f7e6df66e7c5ec1
= <fykhan2002@gmail.com>
2024-04-10 22:26:59 +0800

    attempted session control

--- chat.php
+++ chat.php
@@ -1,10 +1,7 @@
 <?php
-    // Start the session
     session_start();
 
-    // Check if the user is logged in
     if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-        // If the user is not logged in, redirect them to the login page
         header('Location: login.php');
         exit;
     }
